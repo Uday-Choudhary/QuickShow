0f619f59023aae6bdd293f0da9908dd095991a50
Uday-Choudhary <choudharyuday85@gmail.com>
2025-09-24 01:36:49 +0530

    movie section added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
        <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/movies' element={<Movies/>}/>
-        <Route path='/movies/:id ' element={<MovieDetails/>}/>
+        <Route path='/movies/:id' element={<MovieDetails/>}/>
         <Route path='/movies/:id/:date ' element={<SeatLayout/>}/>
         <Route path='/my-bookings' element={<MyBookings/>}/>
         <Route path='/favorite' element={<Favorite/>}/>
--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import { dummyShowsData } from "../assets/assets";
+import MovieCard from "../components/MovieCard";
+import BlurCircle from "../components/BlurCircle";
 
 const Movies = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  if (!dummyShowsData || dummyShowsData.length === 0) {
+    return (
+      <div className="flex items-center justify-center min-h-[80vh]">
+        <p className="text-gray-500 text-lg">No movies available right now.</p>
+      </div>
+    );
+  }
 
-export default Movies
+  return (
+    <div className="relative my-40 mb-60 px-6 md:px-16 lg:px-40 xl:px-44 overflow-hidden min-h-[80vh]">
+      {/* Top Left Blur */}
+      <BlurCircle top="50px" left="0px" />
+
+      {/* Bottom Right Blur */}
+      <BlurCircle bottom="0px" right="0px" />
+
+      <h1 className="text-lg font-medium my-4">Now Showing</h1>
+      <div className="flex flex-wrap max-sm:justify-center gap-8">
+        {dummyShowsData.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Movies;
