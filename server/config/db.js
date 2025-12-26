0ed9bdeaf7b7b00dee863bf06cf13543ad3c59fe
Uday-Choudhary <choudharyuday85@gmail.com>
2025-12-26 22:11:17 +0530

    chore: fix vercel config and ignore .env

---
+++ server/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        mongoose.connection.on("connected", () => {
+            console.log("MongoDB connected");
+        });
+        await mongoose.connect(`${process.env.MONGODB_URI}/quickshow`)
+
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+    }
+};
+
+export default connectDB;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import connectDB from './config/db.js'
+import { clerkMiddleware } from '@clerk/express';
+import { serve } from "inngest/express";
+import { inngest, functions  } from './inngest/index.js';
 
 const app = express();
 const port = 5173;
@@ -9,10 +13,14 @@ const port = 5173;
 //Middleware
 app.use(express.json())
 app.use(cors())
+app.use(clerkMiddleware())
+
+await connectDB()
 
 // API Routes
-app.get('/' , (req , res) => res.send('Server is live'))
+app.get('/', (req, res) => res.send('Server is live'))
+app.use('/api/inngest', serve({client: inngest, functions}));
 
-app.listen(port , () => {
+app.listen(port, () => {
     console.log(`Server is listening at http://localhost:${port}`)
 })
